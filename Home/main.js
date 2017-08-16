b24629a55aebe64fd53a4442d73a490d29876a98
Garrett Rees Hafen <GarrettHafen@ldschurch.org>
2017-08-16 15:38:14 -0600

    added video links, fixed css library

--- Home/main.js
+++ Home/main.js
@@ -1,4 +1,5 @@
 /* Home Page */
+//carousel set up
 carousel = (function(){
   var box = document.querySelector('.carousel');
   var next = box.querySelector('.right');
@@ -31,3 +32,38 @@ carousel = (function(){
   });
   navigate(0);
 })();
+
+//set youtube thumbnails and links
+var test = [1,2,3,4,5];
+var innerDiv = "";
+var channelID = "UCLbmYqqTpfGvCC6qITTnvEw";
+
+for (let i = 0; i< test.length; i++){
+$.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'+channelID, function(data) 
+{
+   var link = data.items[i].link;
+   var id = link.substr(link.indexOf("=")+1);
+    var thumbnailSource = `https://i.ytimg.com/vi/${id}/mqdefault.jpg`;
+    var videoSource = `https://youtube.com/watch?v=${id}`;
+    innerDiv += 
+    ` 
+      <a href="${videoSource}" target = "_blank">
+        <div class="video-box">
+          <img class="video-thumbnail" src="${thumbnailSource}">
+        </div>
+      </a>
+    `
+
+var container = document.getElementById("videos");
+container.innerHTML = innerDiv;
+});
+
+    
+} 
+
+
+/* want to use http://i.ytimg.com/vi/{video_id}/maxresdefault.jpg as the thumbnail finder when i get the href to be the video on youtube
+
+follow xBs website
+
+*/
